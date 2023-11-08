7ff15e90d8533f3707dff7398a7b850521c56027
Mhamed_Azouzi <mhamed.azouzi@neopolis-dev.com>
2023-11-08 18:24:32 +0100

    [SIGNUP] set up and intergration

--- antit_front/src/router/index.js
+++ antit_front/src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/components/LoginView.vue';
+import SignUp from '@/components/SignUp.vue';
 import HelloUser from '@/components/HelloUser.vue';
+import AnnotateAudio from '@/components/AnnotateAudio.vue';
+import AddAudio from '@/components/AddAudio.vue';
 
 const routes = [
   { path: '/login', component: Login },
+  { path: '/signup', component: SignUp },
   { path: '/hello-user', component: HelloUser },
   { path: '/', redirect: '/login' },
 ];
