50fe5050f96980ff7aa6a6351f24a24052e0996f
Mhamed_Azouzi <mhamed.azouzi@neopolis-dev.com>
2023-11-11 11:00:58 +0100

    [AUDIO] add annotated checker

--- antit_back/audio/serializers.py
+++ antit_back/audio/serializers.py
@@ -4,10 +4,24 @@ from audio.models import Audio
 
 class AudioSerializer(ModelSerializer):
     transcription_count = SerializerMethodField()
+    annotated = SerializerMethodField()
 
     class Meta:
         model = Audio
-        fields = ["id", "audio_file", "transcription_count", "duration", "created_at"]
+        fields = [
+            "id",
+            "audio_file",
+            "transcription_count",
+            "annotated",
+            "duration",
+            "created_at",
+        ]
 
     def get_transcription_count(self, obj):
         return obj.transcriptions.count()
+
+    def get_annotated(self, obj):
+        user_id = self.context.get("request").query_params.get("user_id", None)
+        if user_id:
+            return obj.transcriptions.filter(user=user_id).exists()
+        return False
